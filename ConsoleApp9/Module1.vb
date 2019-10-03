d9d3a90d92fa1c980b24bccdd3f6e97991aea0e1
Victor A. Nyangweso <vnyangweso@usiu.ac.ke>
2019-10-03 18:24:08 +0300

    Add project files.

---
+++ ConsoleApp9/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Sum of a and b = " & Addition())
+
+    End Sub
+
+
+    Function Addition() As Integer
+
+        Dim a, b, sum As Integer
+
+        Console.WriteLine("Enter number a")
+        a = Console.ReadLine()
+        Console.WriteLine("Enter number b")
+        b = Console.ReadLine()
+        sum = a + b
+        Return sum
+    End Function
+
+End Module
